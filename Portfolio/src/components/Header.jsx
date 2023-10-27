9b1c8f9076440a28ced24dc0427ae99c68ae85e8
Code Queen <channguyen97@gmail.com>
2023-10-27 09:56:49 -0700

    updated header, added icons, and pic

--- Portfolio/src/components/Header.jsx
+++ Portfolio/src/components/Header.jsx
@@ -1,33 +1,36 @@
 // A single `Header` component that appears on multiple pages
 
-const styles = {
-    headerStyle: {
-        background: 'white',
-        padding: 200,
-        flexDirection: 'row',
-        alignItems: 'flex-start',
-        justifyContent: 'center',
-    },
-    headingStyle: {
-        fontSize: '25px',
-        flexDirection: 'row',
-        alignItems: 'flex-start',
-        justifyContent: 'center',
-    },
-};
+import nameTitle from '../assets/nameTitle.png'
+import subTitle from '../assets/subTitle.png'
+import portfolioPic from '../assets/portfolioPic.png'
+
+
+// const styles = {
+//     headerStyle: {
+//         background: 'white',
+//         padding: 200,
+//         flexDirection: 'row',
+//         alignItems: 'flex-start',
+//         justifyContent: 'center',
+//     },
+//     headingStyle: {
+//         fontSize: '25px',
+//         flexDirection: 'row',
+//         alignItems: 'flex-start',
+//         justifyContent: 'center',
+//     },
+// };
+
 
 
 function Header() {
     return (
-        <header style={styles.headerStyle} className="header">
-            {/* <h1 style={styles.headingStyle}>Welcome</h1> */}
-            {/* <a href="https://fontmeme.com/friends-tv-series-font/"> */}
-                <img src="https://fontmeme.com/permalink/231021/eca262c81d58e8c9185b43cd9e6ef238.png" alt="friends-tv-series-font" border="0" />
-            {/* </a> */}
-            {/* <h1 style={styles.headingStyle}>The One With The Full-Stack Web developer</h1> */}
+        <header className="homeTitle">
 
-                {/* <a href="https://fontmeme.com/friends-tv-series-font/"> */}
-                    <img src="https://fontmeme.com/permalink/231021/7bf56db3c8043e421374105384863efa.png" alt="friends-tv-series-font" border="0" />
+            <img src={nameTitle} alt="header title"/>
+            <img src={subTitle} alt="header subtitle" className="subHeading"/>
+            <img src={portfolioPic} alt="picture" className="portfolioPic"/>
+
         </header>
     );
 }
--- Portfolio/src/components/Navbar.jsx
+++ Portfolio/src/components/Navbar.jsx
@@ -1,7 +1,5 @@
 // A single `Navigation` component within the header that will use `Link` components from `react-router-dom` to conditionally render the different sections of your portfolio
 
-// import { FontAwesomeIcon } from '@fontawesome/react-fontawesome'
-// import { icon } from '@fontawesome/fontawesome-svg-core/import.macro'
 
 const styles = {
     navBarStyle: {
@@ -18,22 +16,37 @@ const styles = {
 };
 
 function Navbar() {
+    // return (
+    //     <section style={styles.navBarStyle} className="navbar font-extrabold" >
+    //         <div style={styles.linkStyle}>
+    //             <a href="#">Home</a>
+    //         </div>
+    //         <div style={styles.linkStyle}>
+    //             <a href="#">Portfolio</a>
+    //         </div>
+    //         <div style={styles.linkStyle}>
+    //             <a href="#">About</a>
+    //         </div>
+    //         <div style={styles.linkStyle}>
+    //             <a href="#">Contact</a>
+    //         </div>
+    //     </section>
+    // );
     return (
-        <section style={styles.navBarStyle} className="navbar font-extrabold" >
-            <div style={styles.linkStyle}>
-                <a href="#">Home</a>
-                {/* <FontAwesomeIcon icon={icon({name: 'house-chimney'})} /> */}
-            </div>
-            <div style={styles.linkStyle}>
-                <a href="#">Portfolio</a>
-            </div>
-            <div style={styles.linkStyle}>
-                <a href="#">About</a>
-            </div>
-            <div style={styles.linkStyle}>
-                <a href="#">Contact</a>
-            </div>
-        </section>
+        <ul className="nav nav-tabs">
+            <li className="nav-item">
+                <img src="../homeicon.svg" className="icon hover:animate-pulse" alt="LinkedIn logo icon"></img>
+            </li>
+            <li className="nav-item">
+                <img src="../abouticon.svg" className="icon hover:animate-pulse" alt="LinkedIn logo icon"></img>
+            </li>
+            <li className="nav-item">
+                <img src="../projecticon.svg" className="icon hover:animate-pulse" alt="LinkedIn logo icon"></img>
+            </li>
+            <li className="nav-item">
+                <img src="../contacticon.svg" className="icon hover:animate-pulse" alt="LinkedIn logo icon"></img>
+            </li>
+        </ul>
     );
 }
 
