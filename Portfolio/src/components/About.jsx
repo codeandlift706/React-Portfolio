990be5fa50877e7a383e96d2e52518b7e3b878b3
Code Queen <channguyen97@gmail.com>
2023-10-31 20:07:49 -0700

    fixed body and root relation, added background image

--- Portfolio/src/components/About.jsx
+++ Portfolio/src/components/About.jsx
@@ -1,14 +1,16 @@
 
 const styles = {
     card: {
-        margin: 20,
+        margin: '5rem',
+        padding: '5rem',
     },
     heading: {
         minHeight: 50,
         pneHeight: 3.5,
         fontSize: '3rem',
-        color: 'white',
-        padding: '0 20px',
+        color: 'pink',
+        padding: '6rem',
+        // backgroundImage: url("cityBackground.png"),
     },
     content: {
         padding: 20,
@@ -17,17 +19,20 @@ const styles = {
 
 function About() {
     return (
-        <div style={styles.card} id="about">
-            <div style={styles.heading}>about me</div>
-            <div style={styles.content}>
-                <p>Orange County, Calfornia.</p> 
-                <p>Dog lover, fantasy football enthusiast, lifter. </p>
+        <>
+            
+            <div style={styles.card} id="about">
+                <div style={styles.heading}>about me</div>
+                <div style={styles.content}>
+                    <p>Orange County, Calfornia.</p>
+                    <p>Dog lover, fantasy football enthusiast, lifter. </p>
                     <p>Aspiring to transition from HR professional into the world of coding, determined to keep learning the programming languages needed to succeed and master web & software development. </p>
-                        <p>Aiming to apply my +4 years of project management experience and continuous improvement mindset to design and enhance user interfaces. </p>
-                        <p></p>
-                    </div>
+                    <p>Aiming to apply my +4 years of project management experience and continuous improvement mindset to design and enhance user interfaces. </p>
+                    <p></p>
+                </div>
             </div>
-            );
+        </>
+    );
 }
 
-            export default About;
+export default About;
