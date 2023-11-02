ab3373e6dc398140fa9c0084462e9fb28e90e2d7
Code Queen <channguyen97@gmail.com>
2023-11-01 18:43:14 -0700

    styled about section

--- Portfolio/src/components/About.jsx
+++ Portfolio/src/components/About.jsx
@@ -15,10 +15,13 @@ const styles = {
         padding: '16rem',
     },
     content: {
-        background: 'white',
+        padding: '5em',
+        fontSize: '1.3rem',
     },
     picture: {
-        background: 'black',
+        // marginLeft: '8rem',
+        borderRadius: '200px',
+        width: '20%',
     },
 };
 
@@ -31,13 +34,15 @@ function About() {
                     <img src={aboutHeading} alt="header title" />
                 </div>
 
-                <img style={styles.picture} src={portfolioPic2} alt="picture" className="portfolioPic" />
-                <div style={styles.content}>
-                    <p>Born in Montreal, Quebec & Raised in Orange County, Calfornia.</p>
-                    <p>Dog lover, fantasy football enthusiast, lifter. </p>
-                    <p>Aspiring to transition from HR professional into the world of coding, determined to keep learning the programming languages needed to succeed and master web & software development. </p>
-                    <p>Aiming to apply my +4 years of project management experience and continuous improvement mindset to design and enhance user interfaces. </p>
-                    <p></p>
+                <div class="aboutContent">
+                    <img style={styles.picture} src={portfolioPic2} alt="picture" />
+                    <div style={styles.content}>
+                        <p>Born in Montreal, Quebec & Raised in Orange County, Calfornia.</p>
+                        <p>Dog lover, fantasy football enthusiast, lifter. </p>
+                        <p>Aspiring to transition from HR professional into the world of coding, determined to keep learning the programming languages needed to succeed and master web & software development. </p>
+                        <p>Aiming to apply my +4 years of project management experience and continuous improvement mindset to design and enhance user interfaces. </p>
+                        <p></p>
+                    </div>
                 </div>
 
             </div>
