6e84d0136fbf13a46bfd8cb859fbbd672d6de307
Code Queen <channguyen97@gmail.com>
2023-11-05 20:46:11 -0800

    fixed project icon and words

--- Portfolio/src/components/Footer.jsx
+++ Portfolio/src/components/Footer.jsx
@@ -20,10 +20,10 @@ function Footer() {
         <section style={styles.footerStyle} className="footer">
 
                 <div style={styles.linkStyle}>
-                <img src="../../githubicon.svg" href="https://github.com/codeandlift706" className="card-icon hover:animate-pulse" alt="Github logo icon"></img>
+                <img src="../../githubicon.svg" href="https://github.com/codeandlift706" className="footer-icon hover:animate-pulse" alt="Github logo icon"></img>
                 </div>
                 <div style={styles.linkStyle}>
-                <img src="../../linkedinicon.svg" href="https://www.linkedin.com/in/chan-nguyen-57184223a/" className="card-icon hover:animate-pulse" alt="LinkedIn logo icon"></img>
+                <img src="../../linkedinicon.svg" href="https://www.linkedin.com/in/chan-nguyen-57184223a/" className="footer-icon hover:animate-pulse" alt="LinkedIn logo icon"></img>
                 </div>
                                 {/* <div style={styles.linkStyle}>
                 <img src="../../chanLogo.png" href="https://www.linkedin.com/in/chan-nguyen-57184223a/" className="cn-logo hover:animate-pulse" alt="LinkedIn logo icon"></img>
