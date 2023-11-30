b1cd041fb6b6e5688506da734a094ca397d6d63c
Code Queen <channguyen97@gmail.com>
2023-11-29 19:09:15 -0800

    cleaned footer code

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,31 +1,17 @@
-const styles = {
-    footerStyle: {
-        display: 'flex',
-        fontFamily: 'helvetica',
-        flexDirection: 'row',
-        // alignItems: 'flex-start',
-        justifyContent: 'center',
-        // background: 'black',
-    },
-    linkStyle: {
-        border: '1px black',
-        paddingTop: '3rem',
-    }
-};
+import github from '../../public/githubicon.svg'
+import linkedin from '../../public/linkedinicon.svg'
+
 
 function Footer() {
     return (
-        <section style={styles.footerStyle} className="footer">
-
-            <div style={styles.linkStyle}>
-                <a href="https://github.com/codeandlift706" target="_blank"><img src="../../githubicon.svg" className="footer-icon hover:animate-pulse" alt="Github logo icon" /></a>
+        <footer className="footer">
+            <div className="footerLink">
+                <a href="https://github.com/codeandlift706" target="_blank"><img src={github} className="footer-icon hover:animate-pulse" alt="Github logo icon" /></a>
             </div>
-
-            <div style={styles.linkStyle}>
-                <a href="https://www.linkedin.com/in/chan-nguyen-57184223a/" target="_blank"><img src="../../linkedinicon.svg" className="footer-icon hover:animate-pulse" alt="Linkedin logo icon" /></a>
+            <div className="footerLink">
+                <a href="https://www.linkedin.com/in/chan-nguyen-57184223a/" target="_blank"><img src={linkedin} className="footer-icon hover:animate-pulse" alt="Linkedin logo icon" /></a>
             </div>
-
-        </section>
+        </footer>
     );
 }
 
