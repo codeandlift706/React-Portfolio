0ee146b133eaeafb4f2459a476c4f9e10feef6d7
Code Queen <channguyen97@gmail.com>
2023-11-29 19:03:24 -0800

    cleaned contact code

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,28 +1,10 @@
 import contactHeading from '../assets/contactHeading.png'
 
-const styles = {
-    content: {
-        // padding: '5rem',
-    },
-    linkStyle: {
-        // border: '1px black',
-        // padding: '5px',
-        textAlign: 'left',
-        paddingLeft: '1rem',
-    },
-    heading: {
-        // minHeight: 50,
-        // pneHeight: 3.5,
-        fontSize: '4rem',
-        color: 'white',
-        padding: '16rem',
-    },
-};
 
 function Contact() {
     return (
-        <section style={styles.contactStyle} className="contact" id="contact">
-            <div style={styles.heading} id="contactHead">
+        <section className="contact">
+            <div className="contactHead">
                 <img src={contactHeading} alt="header title" />
             </div>
 
@@ -33,27 +15,24 @@ function Contact() {
                     <img src="../email.svg" className="contact-icon" alt="email icon" />
                     <img src="../calendar.svg" className="contact-icon" alt="calendar icon" />
                 </div>
-
-                <div style={styles.content}>
-                    <p style={styles.linkStyle}>
+                <div>
+                    <p className="contactLink">
                         Location: California, US
                     </p>
 
-                    <p style={styles.linkStyle}>
+                    <p className="contactLink">
                         Phone: (949) 315-6839
                     </p>
 
-                    <p style={styles.linkStyle}>
+                    <p className="contactLink">
                         < a href="mailto:channguyen715@gmail.com" target="_blank">Email: channguyen715@gmail.com</a>
                     </p>
 
-                    <p style={styles.linkStyle}>
+                    <p className="contactLink">
                         < a href="https://calendly.com/channguyen715/15min" target="_blank">Quick Chat: Set up time via Calendly</a>
                     </p>
                 </div>
             </div>
-
-
         </section>
     );
 }
