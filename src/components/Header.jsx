d7ad3e21030ce65b299f9f8ed4fad63eab031402
Code Queen <channguyen97@gmail.com>
2023-11-29 18:45:24 -0800

    cleaned header code

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,24 +1,12 @@
 import nameTitle from '../assets/nameTitle.png'
 import subTitle from '../assets/subTitle.png'
 
-const styles = {
-    size: {
-        height: '100%',
-        width: '100%',
-    },
-    subsize: {
-        height: '100%',
-        width: '100%',
-    }
-};
-
-
 function Header() {
     return (
-        <header id="home">
+        <header className="home">
             <div className="bothTitles">
-                <img src={nameTitle} style={styles.size} alt="header title" />
-                <img src={subTitle} style={styles.subsize} alt="header subtitle" className="subHeading" />
+                <img src={nameTitle} className="headerTitle" alt="header title" />
+                <img src={subTitle} alt="header subtitle" />
             </div>
         </header>
     );
